50f1a29404a902b0f4ee784c3a5c60abde103eac
riyaz <riyazuddin515@gmail.com>
2021-08-29 11:31:48 +0530

    Initial commit

---
+++ app/src/main/java/com/riyazuddin/zingjetpackcompose/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.riyazuddin.zingjetpackcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val DarkGrey = Color(0xFF202020)
+val White = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+val RedAccent = Color(0xFFFF0036)
+
+val LightGray = Color(0xFF6D6D6D)
